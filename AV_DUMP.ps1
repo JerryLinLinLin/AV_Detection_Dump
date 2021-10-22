6134be8b8532961eb2df5444bb462acaa9f30140
JerryLinLinLin <hlin323@wisc.edu>
2021-10-21 22:08:10 -0500

    First Commit

---
+++ AV_DUMP.ps1
@@ -0,0 +1,100 @@
+<#
+.Synopsis
+	Antivirus Dump
+.DESCRIPTION
+	Dump an Antivirus software's detection name
+.PARAMETER Name
+    Choose an Antivirus: Huorong | Kaspersky | Malwarebytes
+#>
+
+#Requires -Version 4.0
+#Requires -RunAsAdministrator
+
+Param(
+   [Parameter(Mandatory)]
+   [validateset("Huorong","Kaspersky", "Malwarebytes")]
+   [string]$Name
+)
+
+$TimeStamp = (Get-Date).ToString('MM-dd-yyyy-hh-mm-ss')
+New-Item -ItemType Directory -Force $Name
+
+$FileName = "$Name-$TimeStamp"
+$DumpPath = "$FileName.dmp"
+$StringPath = "$FileName.txt"
+
+
+switch ($Name) {
+    Huorong {
+        $Regex = "^[A-Z][^/.=]{0,30}[/][A-Za-z]*[.][^/=]{0,20}$"
+
+        procdump.exe -ma "HipsDaemon.exe" $DumpPath
+        strings.exe -nobanner -n 7 "$FileName.dmp" > $StringPath
+
+        $OutputPath = "$FileName-BASE.txt"
+        select-string -Path $StringPath -Pattern $Regex -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } > $OutputPath
+
+        $ResultPath = "$Name/$FileName-BASE.csv"
+        Get-Content $OutputPath | Sort-Object | get-unique > $ResultPath
+
+        Remove-Item $OutputPath
+    }
+    Kaspersky {
+        $Regex_BASE = "^(?=[A-Z])(?!.*System.*|.*Windows.*|.*Microsoft.*|.*SCC.*|.*CLN.*|.*PDM.*|.*=.*|.*\\.*|.*//.*|.*\(.*)([^.]*)\.([^.]{1,10})\.([^.]*)\..*"
+        $Regex_PDM = "^PDM:.*"
+
+        procdump.exe -ma "avp.exe" $DumpPath
+        strings.exe -nobanner -n 7 "$FileName.dmp" > $StringPath
+
+        $OutputPath_BASE = "$FileName-BASE.txt"
+        $OutputPath_PDM = "$FileName-PDM.txt"
+        select-string -Path $StringPath -Pattern $Regex_BASE -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } > $OutputPath_BASE
+        select-string -Path $StringPath -Pattern $Regex_PDM -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } > $OutputPath_PDM
+
+        $ResultPath_BASE = "$Name/$FileName-BASE.csv"
+        $ResultPath_PDM = "$Name/$FileName-PDM.csv"
+        Get-Content $OutputPath_BASE | Sort-Object | get-unique > $ResultPath_BASE
+        Get-Content $OutputPath_PDM | Sort-Object | get-unique > $ResultPath_PDM
+
+        Remove-Item $OutputPath_BASE
+        Remove-Item $OutputPath_PDM
+        
+    }
+    Malwarebytes {
+        $Regex_DDS = ".*.DDS$"
+
+        procdump.exe -ma "MBAMService.exe" $DumpPath
+        strings.exe -nobanner -u -n 7 "$FileName.dmp" > $StringPath
+
+        $OutputPath_DDS = "$FileName-DDS.txt"
+        $OutputPath_BASE = "$FileName-BASE.txt"
+        select-string -Path $StringPath -Pattern $Regex_DDS -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } > $OutputPath_DDS
+        
+        $Range = $false
+        Get-Content -Path $StringPath | Foreach-Object {
+            if($_ -match "PUP.Optional.Bandoo.AppFlsh"){
+                $Range = $true
+            }
+            if($_ -match "Toshiba.MBR"){
+                $Range = $false
+            }
+            if ($Range) {
+                Add-Content $OutputPath_BASE $_ 
+            }
+        }
+
+        $ResultPath_DDS = "$Name/$FileName-DDS.csv"
+        $ResultPath_BASE = "$Name/$FileName-BASE.csv"
+        Get-Content $OutputPath_DDS | Sort-Object | get-unique > $ResultPath_DDS
+        Get-Content $OutputPath_BASE | Sort-Object | get-unique > $ResultPath_BASE
+
+        Remove-Item $OutputPath_BASE
+        Remove-Item $OutputPath_DDS
+    }
+    Default {
+        return
+    }
+}
+
+Remove-Item $DumpPath
+Remove-Item $StringPath
---
+++ README.md
@@ -0,0 +1,39 @@
+# Antivirus Dump
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [List of Supported Vendors](#list_of_vendors)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+This project contains the CSV files of malware detection names from Antivirus software, and a PowerShell script for dumping the detection entries.
+
+## Getting Started <a name = "getting_started"></a>
+
+Each subfolder contains dump CSV files with vendor's name and date. File name ends with BASE contains names from a vendor's scan engine, and others may be different depended on the sources of detection (e.g. behavior protection).
+
+### Prerequisites
+
+To run PowerShell Script
+
+1. Download Windows Sysinternals and add to `PATH` or install from [Microsoft Store](https://www.microsoft.com/en-us/p/sysinternals-suite/9p7knl5rwt25).
+
+2. Disable PPL(Protected Processes Light) using [PPLKiller](https://github.com/Mattiwatti/PPLKiller).
+
+3. Disable Self-Protection Module of AV if possible.
+
+## Usage <a name = "usage"></a>
+
+`powershell -executionpolicy bypass -File .\AV_DUMP.ps1 $Name`
+
+## List of Supported Vendors <a name = "list_of_vendors"></a>
+
+| Name         | PPL | Need to Disable SP | Detection Source |
+| ------------ | --- | ------------------ | ---------------- |
+| Huorong      | No  | No                 | BASE             |
+| Kaspersky    | Yes | Yes                | BASE, PDM        |
+| Malwarebytes | Yes | No                 | BASE, DDS        |
